4bc64955fbf315229336c84d1249ebc933651edc
yatsun <yatsunlam25@gmail.com>
2021-10-06 04:13:17 -0400

    added destroy

--- app/controllers/sandwichshops_controller.rb
+++ app/controllers/sandwichshops_controller.rb
@@ -7,12 +7,33 @@ class SandwichshopsController < ApplicationController
 
     def create
         sandwichshop = Sandwichshop.new(sandwichshop_params)
+        
+        if sandwichshop.save
+            render json: sandwichshop
+        else
+            render json: {error: "Couldnt be saved"}
+        end
+    end
+
+    def update
+        sandwichshop = Sandwichshop.find_by_id(params[:id])
+        if sandwichshop.update(sandwichshop_params)
+            render json: sandwichshop
+        else
+            render json: {error: "Couldn't update"}
+        end
+    end
+
+    def destroy
+        sandwichshop = Sandwichshop.find_by_id(params[:id])
+        sandwichshop.destroy
+        render json: {message: "Successfully deleted #{sandwichshop.name}"}
     end
 
     private
 
     def sandwichshop_params
-        
+        params.require(:sandwichshop).permit(:name, :description, :city_id)
     end
 
 end
